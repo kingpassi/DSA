ebb25c36b128de38765de61b8ca757feac050e48
YASH PASSI <98830689+kingpassi@users.noreply.github.com>
2022-05-11 23:15:49 +0530

    Add files via upload

---
+++ Fibonacci Number(Recursion).cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+Fibonacci Number
+
+Example 1:
+
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+
+Example 2:
+
+Input: n = 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+*******************************************************************************/
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+        return fib(n-1)+fib(n-2);
+    }
+};
